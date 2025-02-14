2c76f6cc5d4e824db29356bdd7c556a46a779787
Natalia Bespalova <nbespalova@gmail.com>
2025-02-14 12:16:00 +0200

    first

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(('blog.urls', 'blog'), namespace='main_blog')),  # Задаем уникальный namespace
+    path('pages/', include(('pages.urls', 'pages'), namespace='pages_app')),
+    path('blog/', include(('blog.urls', 'blog'), namespace='blog_section')),
+]
